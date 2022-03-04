e0d7ba3c99d6416d0614592c4b8b4b2b60eb5e33
spatialfree <cadetmerchant@gmail.com>
2022-03-04 10:55:24 -0500

    democrandom

--- include/includer.js
+++ include/includer.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   var nav = `<div style="display: flex; justify-content: space-between; align-items: center; border-bottom: 2px solid #666; background: gray;">`
-  nav += `<a href="javascript:javascript:history.go(-1)">`
+  nav += `<a href="/">` // javascript:javascript:history.go(-1)
   nav += `<img src="/res/home-icon.svg" style="margin: 1rem; width: 32px; height: 32px;" />`
   nav += `</a>`
   nav += `</div>`
